5a02cbdc82b790d9c6c1c72b5cdcc750e764d4c0
tommberg <danieltomberg.work@gmail.com>
2025-03-06 16:30:08 +0200

    Added UI for login and registration

--- server/index.js
+++ server/index.js
@@ -1,24 +1,29 @@
-const express = require("express")
-const mongoose = require("mongoose")
-const config = require("config")
-const authRouter = require("./routes/auth.routes")
-const app = express()
-const PORT = config.get('serverPort')
+const express = require("express");
+const mongoose = require("mongoose");
+const config = require("config");
+const authRouter = require("./routes/auth.routes");
+const corsMiddleware = require("./middleware/cors.middleware");
 
-app.use(express.json())
-app.use("/api/auth", authRouter)
+const app = express();
+const PORT = config.get("serverPort") || 5003;
 
+app.use(corsMiddleware);
+app.use(express.json());
+app.use("/api/auth", authRouter);
 
 const start = async () => {
     try {
-        await mongoose.connect(config.get("dbUrl"))
+        await mongoose.connect(config.get("dbUrl"), {
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        });
+        console.log("Connected to MongoDB");
 
-        app.listen(PORT, () => {
-            console.log('Server started on port ', PORT)
-        })
+        app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
     } catch (e) {
-
+        console.error("MongoDB Connection Error:", e);
+        process.exit(1);
     }
-}
+};
 
-start()
+start();
