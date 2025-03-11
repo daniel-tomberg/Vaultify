6da0c84493a2c7a0523551b3ee2df1e5b6a34c35
tommberg <danieltomberg.work@gmail.com>
2025-03-11 19:43:44 +0200

    added upload file & drag and drop frontend

--- client/src/components/disk/Disk.jsx
+++ client/src/components/disk/Disk.jsx
@@ -1,6 +1,6 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useDispatch, useSelector} from "react-redux";
-import {getFiles} from "../../actions/file";
+import {getFiles, uploadFile} from "../../actions/file";
 import FileList from "./fileList/FileList";
 import './disk.css'
 import Popup from "./Popup";
@@ -10,6 +10,7 @@ const Disk = () => {
     const dispatch = useDispatch()
     const currentDir = useSelector(state => state.files.currentDir)
     const dirStack = useSelector(state => state.files.dirStack)
+    const [dragEnter, setDragEnter] = useState(false);
 
     useEffect(() => {
         dispatch(getFiles(currentDir))
@@ -24,15 +25,51 @@ const Disk = () => {
         dispatch(setCurrentDir(backDirId))
     }
 
-    return (
-        <div className="disk">
-            <div className="disk__btns">
-                <button className="disk__back" onClick={() => backClickHandler()}>Back</button>
-                <button className="disk__create" onClick={() => showPopupHandler()}>Create folder</button>
+    function fileUploadHandler(event) {
+        const files = [...event.target.files]
+        files.forEach(file => dispatch(uploadFile(file, currentDir)))
+    }
+
+    function dragEnterHandler(event) {
+        event.preventDefault()
+        event.stopPropagation()
+        setDragEnter(true)
+    }
+
+    function dragLeaveHandler(event) {
+        event.preventDefault()
+        event.stopPropagation()
+        setDragEnter(false)
+    }
+
+    function dropHandler(event) {
+        event.preventDefault()
+        event.stopPropagation()
+        let files = [...event.dataTransfer.files]
+        files.forEach(file => dispatch(uploadFile(file, currentDir)))
+        setDragEnter(false)
+    }
+
+    return (!dragEnter ?
+            <div className="disk" onDragEnter={dragEnterHandler} onDragLeave={dragLeaveHandler}
+                 onDragOver={dragEnterHandler}>
+                <div className="disk__btns">
+                    <div className="disk__upload">
+                        <label htmlFor="disk__upload-input" className="disk__upload-label">Upload or drop</label>
+                        <input multiple={true} onChange={(event) => fileUploadHandler(event)} type="file"
+                               id="disk__upload-input" className="disk__upload-input"/>
+                    </div>
+                    <button className="disk__back" onClick={() => backClickHandler()}>Back</button>
+                    <button className="disk__create" onClick={() => showPopupHandler()}>Create folder</button>
+                </div>
+                <FileList/>
+                <Popup/>
             </div>
-            <FileList/>
-            <Popup/>
-        </div>
+            :
+            <div className="drop-area" onDrop={dropHandler} onDragEnter={dragEnterHandler}
+                 onDragLeave={dragLeaveHandler} onDragOver={dragEnterHandler}>
+                Drop files here
+            </div>
     );
 };
 
--- client/src/components/disk/fileList/file/File.jsx
+++ client/src/components/disk/fileList/file/File.jsx
@@ -3,20 +3,27 @@ import './file.css'
 
 import dirLogo from '../../../../assets/img/folder3.svg'
 import fileLogo from '../../../../assets/img/file3.svg'
+import imgFileLogo from '../../../../assets/img/imgFile3.svg'
 import {useDispatch, useSelector} from "react-redux";
 import {pushToStack, setCurrentDir} from "../../../../reducers/fileReducer";
 const File = ({file}) => {
     const dispatch = useDispatch();
     const currentDir = useSelector(state => state.files.currentDir);
 
-    function openDirHandler(){
-        dispatch(pushToStack(currentDir));
-        dispatch(setCurrentDir(file._id));
+    function openDirHandler(file){
+        if(file.type === 'dir'){
+            dispatch(pushToStack(currentDir));
+            dispatch(setCurrentDir(file._id));
+        }
     }
 
     return (
-        <div className='file' onClick={file.type === 'dir' ? ()=> openDirHandler() : ''}>
-            <img src={file.type === 'dir' ? dirLogo : fileLogo} alt="" className="file__img"/>
+        <div className='file' onClick={()=> openDirHandler(file)}>
+            <img
+                src={file.type === 'dir' ? dirLogo : (file.type === 'jpg' || file.type === 'png') ? imgFileLogo : fileLogo}
+                alt=""
+                className="file__img"
+            />
             <div className="file__name">{file.name}</div>
             <div className="file__date">{file.date?.slice(0, 10) || "Unknown"}</div>
             <div className="file__size">{file.size}</div>
